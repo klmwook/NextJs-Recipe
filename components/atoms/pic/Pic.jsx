c49276da7adfba38236076923e4d4d818d3c73b6
klmwook <klmwook72@gmail.com>
2023-08-18 10:23:44 +0900

    이미지 캐싱되기 전까지 로딩바 처리

--- components/atoms/pic/Pic.jsx
+++ components/atoms/pic/Pic.jsx
@@ -3,12 +3,14 @@ import clsx from 'clsx';
 import styles from './Pic.module.scss';
 import Link from 'next/link';
 import { RingLoader } from 'react-spinners';
+import { useState } from 'react';
 
 //react-spinners
 export function Pic({ imgSrc, style, imgTxt, children, className, priority = false, url }) {
+	const [IsLoaded, setIsLoaded] = useState(false);
 	return (
 		<div className={clsx(styles.pic, className)} style={style}>
-			<Image src={imgSrc} alt={imgSrc} priority={priority} fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' />
+			<Image src={imgSrc} alt={imgSrc} priority={priority} fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' onLoadingComplete={() => setIsLoaded(true)} />
 
 			{imgTxt && (
 				<>
@@ -30,7 +32,17 @@ export function Pic({ imgSrc, style, imgTxt, children, className, priority = fal
 			)}
 
 			{/* spinner로딩 */}
-			<RingLoader cssOverride={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%)' }} size={100} color={'aqua'} />
+			<RingLoader
+				cssOverride={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%,-50%)',
+				}}
+				size={100}
+				color={'aqua'}
+				loading={!IsLoaded}
+			/>
 		</div>
 	);
 }
