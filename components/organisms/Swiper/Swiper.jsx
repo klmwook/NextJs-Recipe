c9bab9789e3ad52272b1fe5256300a9ef1b331f2
klmwook <klmwook72@gmail.com>
2023-08-21 10:30:10 +0900

    데이터 Swiper 컴포넌트로 전달

---
+++ components/organisms/Swiper/Swiper.jsx
@@ -0,0 +1,15 @@
+import styles from './Swiper.module.scss';
+import clsx from 'clsx';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/css';
+
+//npm i swiper@9
+function SwiperWrap({ recipe, category }) {
+	console.log('recipe', recipe);
+	console.log('category', category);
+
+	return <figure></figure>;
+}
+
+export default SwiperWrap;
