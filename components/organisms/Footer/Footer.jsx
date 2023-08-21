d782d64548e455001637fa439f2c868b9bcc989b
klmwook <klmwook72@gmail.com>
2023-08-21 10:10:53 +0900

    Footer 적용

---
+++ components/organisms/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import Title from '@/components/atoms/text/Title';
+import Text from '@/components/atoms/text/Text';
+import clsx from 'clsx';
+import styles from './Footer.module.scss';
+
+function Footer() {
+	return (
+		<footer className={clsx(styles.footer)}>
+			<Title style={{ fontSize: 20, color: '#777' }}>DCODELAB</Title>
+			<Text type={'util'} style={{ letterSpacing: 2 }}>
+				2023 Decodelab All rightsreserved.
+			</Text>
+		</footer>
+	);
+}
+
+export default Footer;
