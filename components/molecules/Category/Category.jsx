fba710605738968491705bb5c64ce8e6ed4582b8
klmwook <klmwook72@gmail.com>
2023-08-22 11:56:40 +0900

    리액트쿼리 커스텀훅 변경요청 이벤트 방법

--- components/molecules/Category/Category.jsx
+++ components/molecules/Category/Category.jsx
@@ -2,11 +2,11 @@ import Btn from '@/components/atoms/Button/Btn';
 import styles from './Category.module.scss';
 import clsx from 'clsx';
 
-function Category({ items }) {
+function Category({ items, handler }) {
 	return (
 		<nav className={clsx(styles.category)}>
 			{items.map((el) => (
-				<Btn key={el.idCategory} type={'button'}>
+				<Btn key={el.idCategory} handler={handler}>
 					{el.strCategory}
 				</Btn>
 			))}
--- pages/find-recipe/index.jsx
+++ pages/find-recipe/index.jsx
@@ -2,13 +2,21 @@ import Head from 'next/head';
 import styles from './style.module.scss';
 import axios from 'axios';
 import Category from '@/components/molecules/Category/Category';
+import { useRecipeByCategory } from '@/hooks/useRecipe';
+import { useState } from 'react';
 
 export default function Recipe({ categories }) {
 	// idCategory
 	// strCategory
 	// strCategoryDescription
 	// strCategoryThumb
-	console.log(categories);
+
+	//react-query를 활용하는 쿼리키 인수값을 State에 담음
+	const [Selected, setSelected] = useState(categories[0].strCategory);
+	//해당 State값이 바뀔때마다 react-query훅이 호출되면서 새로운 데이터 패칭
+	const { data, isSuccess } = useRecipeByCategory(Selected);
+	console.log(data);
+
 	return (
 		<>
 			<Head>
@@ -17,7 +25,8 @@ export default function Recipe({ categories }) {
 
 			<section className={styles.recipePage}>
 				<nav>
-					<Category items={categories} />
+					{/* 이벤트가 발생해야 되는 자식요소에 queryKey로 연동되어 있는 state변경함수를 전달 */}
+					<Category items={categories} handler={setSelected} />
 				</nav>
 			</section>
 		</>
