f97d9ca55159b13d1405981d8961b943dd08fd05
klmwook <klmwook72@gmail.com>
2023-08-22 12:26:51 +0900

    자식컴포넌트에 이벤트 핸들러전달할때 무조건 이벤트명으로 전달하도록 명시

--- components/molecules/Category/Category.jsx
+++ components/molecules/Category/Category.jsx
@@ -2,11 +2,13 @@ import Btn from '@/components/atoms/Button/Btn';
 import styles from './Category.module.scss';
 import clsx from 'clsx';
 
-function Category({ items, handler }) {
+function Category({ items, onClick }) {
 	return (
 		<nav className={clsx(styles.category)}>
 			{items.map((el) => (
-				<Btn key={el.idCategory} handler={handler}>
+				// 부모로부터 받은 onClick 핸들러 함수 props을 다시 Btn컴포넌트에 전달
+				// onClick 이벤트에 전달받은 핸들러 함수 연결
+				<Btn key={el.idCategory} onClick={() => onClick(el.strCategory)}>
 					{el.strCategory}
 				</Btn>
 			))}
