46b979ebbf5963293ccd8ebbe1168d3ca690944a
klmwook <klmwook72@gmail.com>
2023-08-22 11:12:58 +0900

    react-query커스텀 훅 추가

---
+++ hooks/useRecipe.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const getRecipeByCategory = async ({ queryKey }) => {
+	const { data } = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${queryKey[1]}`);
+	//해당 커스텀훅으로 훛ㄹ되는 fetching 함수가 만약 컴포넌트 마운트되자마자 호출된다면
+	//data값 자체가 없기 때문에 meals에서 undefined 오류 발생을 피하기 위함
+	return data.meals;
+};
+
+export const useRecipeByCategory = (SelectedCategory) => {
+	return useQuery(['RecipeByCategory', SelectedCategory], getRecipeByCategory, {
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		cacheTime: 0,
+		staleTime: 0,
+	});
+};
