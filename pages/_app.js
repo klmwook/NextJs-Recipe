792cb7c6b323228443f3e3e9346aee4664b07bf5
klmwook <klmwook72@gmail.com>
2023-08-22 10:48:20 +0900

    리액트 쿼리 세팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import Layout from '@/components/templete/Layout/Layout';
 import '@/styles/globals.scss';
 import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools/build/lib/devtools';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<QueryClientProvider client={queryClient}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
