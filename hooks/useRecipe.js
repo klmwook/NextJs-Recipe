9401409bcbf2a25450aa640e697c5ba192fc507f
klmwook <klmwook72@gmail.com>
2023-08-23 10:04:37 +0900

    csr방식에서 발생할 수 있는 오류 처리

--- hooks/useRecipe.js
+++ hooks/useRecipe.js
@@ -16,6 +16,9 @@ export const useRecipeByCategory = (SelectedCategory) => {
 		cacheTime: 0,
 		staleTime: 0,
 		retry: 3, //데이터 요청 시도 횟수 (default : 3, 네트워크 상황이 안좋을 때 재시도 횟수 늘림)
-		enabled: true, //useQuery 호출 유무 true(실행) false(실행 안함)(default : true)
+		//enabled 값에는 truthy,falsy값이 적용 안됨 (직접 boolean 값을 생성해서 지정)
+		//지금 상황에서는 SSG방식으로 초기 데이터를 호출하고 있기 때문에 아래 구문을 지정 안해도 잘 동작 됨
+		//CSR 방식으로 호출 할 때에는 초기값이 undefined이기 때문에 발생하는 에러를 미리 방지
+		enabled: SelectedCategory !== undefined, //useQuery 호출 유무 true(실행) false(실행 안함)(default : true)
 	});
 };
