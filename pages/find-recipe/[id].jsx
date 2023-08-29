5df04499e9b26dcaca7283ffe46fba2941e7fe99
klmwook <klmwook72@gmail.com>
2023-08-29 10:52:54 +0900

    다이나믹 router에 페이지 트랜지션 모션 적용시 페이지 변경될때 prop 오류 뜨는 이유와 해결방법

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -36,6 +36,7 @@ function Detail() {
 				//이후 치환된 +기준으로 뒤에 값만 map으로 리턴
 				//특정 레시피에는 .\t가 없는 문장도 있기 때문에 해당 구분점이 없을떄는 기존 text를 리턴 그렇지 않으면 치환해서 리턴
 				.map((text) => (text.includes('.\t') ? text.replace('.\t', '+').split('+')[1] : text))
+				//.map((text) => (/\d[' ']/.test(text) ? text.replace(/\d[' ']/, '+').split('+')[1] : text))
 				//원본 문자열에 줄바꿈 정규 표현식이 여러번 들어가 있는 문장의 경우는 빈 문장을 배열로 반환하기 때문에 해당 배열값을 제거
 				.filter((text) => text !== '');
 			console.log(intructions);
@@ -45,8 +46,17 @@ function Detail() {
 
 	return (
 		<section className={clsx(styles.detail)}>
-			<ClimbingBoxLoader loading={!isSuccess} cssOverride={{ position: 'absolute', top: 350, left: '50%', transform: 'translateX(-50%)' }} color={'orange'} size={100} />
-			{isSuccess && (
+			<ClimbingBoxLoader
+				//기본적으로 next는 라우터명이 변경될 때 마다 언마운트되는 페이지 컴포넌트의 csr방식으로 가져온 데이터와 스타일 노드를 제거
+				//page transition이 적용되어 있기 떄문에 상세페이지에서 다른 페이지로 넘어갈때 데이터는 이미 사라졌음에도 불구하고 데이터를 활용하는 컴포넌트가 계속 있으면 prop오류 발생
+				//해결방법 : csr 방식으로 가져오는 데이터 자체를 컴포넌트 렌더링의 조건설정
+				//데이터가 없으면 로딩바 출력, 데이터가 있으면 그 데이터를 활용하는 컴포넌트 출력
+				loading={!data}
+				cssOverride={{ position: 'absolute', top: 350, left: '50%', transform: 'translateX(-50%)' }}
+				color={'orange'}
+				size={100}
+			/>
+			{data && (
 				<>
 					<Title type={'slogan'}>{data.strMeal}</Title>
 
