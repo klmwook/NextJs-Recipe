3f6e6c738fac59df89b4335a648dbfd428c5e148
klmwook <klmwook72@gmail.com>
2023-08-29 11:13:57 +0900

    상세페이지 접속시 로컬저장소의 유무를 파악한다음 해당 저장소에 레시피 아이디가 있는지를 판단해서 state에 저장

--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -15,6 +15,27 @@ function Detail() {
 	const { data, isSuccess } = useRecipeById(id);
 	const [TableData, setTableData] = useState([]);
 	const [ListData, setListData] = useState([]);
+	const [Saved, setSaved] = useState(false);
+
+	//router로 들어오는 id값이 변경될 때 마다 실행되는 useEffect
+	useEffect(() => {
+		//로컬 저장소에 saveRecipe이름으로 특정 값이 있기만 하면 실행
+		if (localStorage.getItem('saveRecipe')) {
+			//해당 데이터를 배열로 파싱해서 가져옴
+			const savedRecipe = JSON.parse(localStorage.getItem('savedRecipe'));
+
+			//가져온 배열값에서 router들어온 id값이 있는지 확인
+			if (savedRecipe.includes(id)) {
+				setSaved(true);
+				//로컬저장소에 값은 있지만 현재 라우터로 받은 레시피 정보값은 없는 경우
+			} else {
+				setSaved(false);
+			}
+			//아예 로컬저장소 자체가 없으면 그냥 빈 배열값으로 저장소 생성
+		} else {
+			localStorage.setItem('savedRecipe', JSON.stringify([]));
+		}
+	}, [id]);
 
 	useEffect(() => {
 		if (data) {
