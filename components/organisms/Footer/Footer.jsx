afa7ee66ba79fedd25e724bc7d7bc8d2d7c5e3ad
klmwook <klmwook72@gmail.com>
2023-08-31 10:57:17 +0900

    버튼 클릭시 실시간으로 테마 색상 변경

--- components/organisms/Footer/Footer.jsx
+++ components/organisms/Footer/Footer.jsx
@@ -4,14 +4,19 @@ import clsx from 'clsx';
 import styles from './Footer.module.scss';
 import { BounceLoader } from 'react-spinners';
 import Btn from '@/components/atoms/Button/Btn';
+import { useGlobalData } from '@/hooks/useGlobalContext';
 
 function Footer() {
+	const { setTheme } = useGlobalData();
+
 	return (
 		<footer className={clsx(styles.footer)}>
 			<nav>
-				<Btn>Orange</Btn>
-				<Btn>Hotpink</Btn>
-				<Btn>Aqua</Btn>
+				{['Orange', 'Aqua', 'Hotpink'].map((el, idx) => (
+					<Btn key={idx} onClick={() => setTheme(`theme${idx + 1}`)}>
+						{el}
+					</Btn>
+				))}
 			</nav>
 
 			<Text type={'util'} style={{ letterSpacing: 2 }}>
