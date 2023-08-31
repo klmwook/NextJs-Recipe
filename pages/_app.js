70a4ec16566f5be5f70831bbe6e21442ed14b2df
klmwook <klmwook72@gmail.com>
2023-08-31 11:05:41 +0900

    페이지 전환 컴포넌트를 레이아웃으로 위치 변경

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useRouter } from 'next/router';
-import { motion, AnimatePresence } from 'framer-motion';
+
 import { keepStyle } from '@/ilbs/keepStyle';
 import { GlobalProvider } from '@/hooks/useGlobalContext';
 
@@ -18,29 +18,9 @@ export default function App({ Component, pageProps }) {
 	return (
 		<GlobalProvider>
 			<QueryClientProvider client={queryClient}>
-				<AnimatePresence mode='wait'>
-					<motion.div key={router.pathname}>
-						<Layout>
-							<Component {...pageProps} />
-						</Layout>
-						{/* 페이지가 바뀌깃 시작할 때 나타날 프레임 */}
-						<motion.div
-							className='in'
-							initial={{ scaleX: 0 }}
-							animate={{ scaleX: 0 }}
-							exit={{ scaleX: 1 }}
-							transition={{ duration: 0.7, ease: [0.26, 0.11, 0, 0.98] }}
-						></motion.div>
-						{/* 페이지가 바뀌고 나서 사라질 프레임 */}
-						<motion.div
-							className='out'
-							initial={{ scaleX: 1 }}
-							animate={{ scaleX: 0 }}
-							exit={{ scaleX: 0 }}
-							transition={{ duration: 0.7, ease: [0.26, 0.11, 0, 0.98] }}
-						></motion.div>
-					</motion.div>
-				</AnimatePresence>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
 				<ReactQueryDevtools />
 			</QueryClientProvider>
 		</GlobalProvider>
